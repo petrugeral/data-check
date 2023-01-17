8d5a6bb8ec91941cb8fcb5616e65e0a0484adb5d
giovane petruccelli <petrugeral@gmail.com>
2023-01-16 22:23:43 -0300

    Solved the first challenge of the bootcamp:)

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
 """Dummy challenge for Kitt Demo"""
 
-
-
+from math import pi
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
     pass  # YOUR CODE HERE
+    if radius > 0:
+        return(pi*radius*radius)
+    else:
+        return 0
